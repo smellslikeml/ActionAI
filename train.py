f1448dc3670bcd8d26ef1a5c8c42ae4c3a7373c7
mayorquinmachines <smayorquin@berkeley.edu>
2019-11-13 13:53:42 -0800

    simplified train.py script

--- train.py
+++ train.py
@@ -3,7 +3,13 @@ from sklearn.pipeline import Pipeline
 
 from transformer import PoseExtractor
 
-def train_model(csv_path, pipeline):
+def actionModel(classifier)
+    pipeline = Pipeline([
+               ('pose_extractor', PoseExtractor()),
+               ('classifier', classifier)])
+    return pipeline
+
+def trainModel(csv_path, pipeline):
     df = pd.read_csv(csv_path)
     X = df['image'].values
     y = df['label']
@@ -21,11 +27,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     config = importlib.import_module('config.' + args.config)
 
-    pipeline = Pipeline([
-    ('pose_extractor', PoseExtractor()),
-    ('classifier', config.classifier())])
-
-    model = train_model(config.csv_path, pipeline)
+    pipeline = actionModel(config.classifier())
+    model = trainModel(config.csv_path, pipeline)
 
     # Dump the model to file
     pickle.dump(model, open(config.classifier_model, 'wb'), protocol=2)

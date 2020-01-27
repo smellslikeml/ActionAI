991567d5a91ce529dffd50ab7e5729a22f81b8c3
smellslikeml <trollofberkeley@gmail.com>
2020-01-27 08:09:20 -0800

    draw objs

--- experimental/poses.py
+++ experimental/poses.py
@@ -32,37 +32,28 @@ class DrawObjects(object):
         self.topology = topology
         self.body_labels = cfg.body_dict 
 
-    def __call__(self, image, object_counts, obj, normalized_peaks, color):
+    def __call__(self, image, object_counts, objects, normalized_peaks):
         topology = self.topology
         height = image.shape[0]
         width = image.shape[1]
 
         K = topology.shape[0]
         count = int(object_counts[0])
-        body_dict = {}
-        C = obj.shape[0]
-        for j in range(C):
-            k = int(obj[j])
-            if k >= 0:
-                peak = normalized_peaks[0][j][k]
-                x = round(float(peak[1]) * width)
-                y = round(float(peak[0]) * height)
-                cv2.circle(image, (x, y), 3, color, 2)
-                body_dict[self.body_labels[j]] = (x,y)
+        body_list = []
+        for obj in objects[0]:
+            body_dict = {}
+            C = obj.shape[0]
+            for j in range(C):
+                k = int(obj[j])
+                if k >= 0:
+                    peak = normalized_peaks[0][j][k]
+                    x = round(float(peak[1]) * width)
+                    y = round(float(peak[0]) * height)
+                    #cv2.circle(image, (x, y), 3, color, 2)
+                    body_dict[self.body_labels[j]] = (x,y)
+            body_list.append(body_dict)
 
-        for k in range(K):
-            c_a = topology[k][2]
-            c_b = topology[k][3]
-            if obj[c_a] >= 0 and obj[c_b] >= 0:
-                peak0 = normalized_peaks[0][c_a][obj[c_a]]
-                peak1 = normalized_peaks[0][c_b][obj[c_b]]
-                x0 = round(float(peak0[1]) * width)
-                y0 = round(float(peak0[0]) * height)
-                x1 = round(float(peak1[1]) * width)
-                y1 = round(float(peak1[0]) * height)
-                cv2.line(image, (x0, y0), (x1, y1), color, 2)
-
-        return body_dict
+        return body_list
 
 topology = trt_pose.coco.coco_category_to_topology(human_pose)
 parse_objects = ParseObjects(topology)
@@ -83,9 +74,7 @@ def inference(image):
     cmap, paf = model_trt(data)
     cmap, paf = cmap.detach().cpu(), paf.detach().cpu()
     counts, objects, peaks = parse_objects(cmap, paf) #, cmap_threshold=0.15, link_threshold=0.15)
-    color = (112,107,222)  # make dictionary from obj id to cmap
-    body_list = []
-    for obj in objects[0]:
-        body_list.append(draw_objects(image, counts, obj, peaks, color))
+    #color = (112,107,222)  # make dictionary from obj id to cmap
+    body_list = draw_objects(image, counts, objects, peaks)
     return image, body_list
 
